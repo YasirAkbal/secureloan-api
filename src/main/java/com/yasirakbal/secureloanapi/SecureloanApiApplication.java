69fe281621740976bc0ca046e2db5e47ea238039
YasirAkbal <yasir.akbal1997@hotmail.com>
2026-02-01 16:18:00 +0300

    First Commit

---
+++ src/main/java/com/yasirakbal/secureloanapi/SecureloanApiApplication.java
@@ -0,0 +1,13 @@
+package com.yasirakbal.secureloanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureloanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureloanApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasirakbal/secureloanapi/SecureloanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasirakbal.secureloanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureloanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
