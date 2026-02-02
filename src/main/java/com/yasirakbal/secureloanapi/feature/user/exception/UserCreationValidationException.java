0cac88d4871c2ecdbd3c842d723672661d09ba7e
YasirAkbal <yasir.akbal1997@hotmail.com>
2026-02-03 00:18:46 +0300

    Fix stack trace exposure in user registration

--- src/main/java/com/yasirakbal/secureloanapi/feature/user/exception/UserCreationValidationException.java
+++ src/main/java/com/yasirakbal/secureloanapi/feature/user/exception/UserCreationValidationException.java
@@ -3,31 +3,36 @@ package com.yasirakbal.secureloanapi.feature.user.exception;
 import com.yasirakbal.secureloanapi.common.exception.BusinessException;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Map;
 
 public class UserCreationValidationException extends BusinessException {
-    @Getter
-    private final List<BusinessException> creationErrors;
 
     public UserCreationValidationException(List<BusinessException> creationErrors) {
-        this(creationErrors, "User creation failed.", HttpStatus.CONFLICT);
+        this(creationErrors, "User creation failed", HttpStatus.CONFLICT);
     }
 
     public UserCreationValidationException(List<BusinessException> creationErrors, String message, HttpStatus statusCode) {
         super(message, statusCode);
-        this.creationErrors = creationErrors;
-        addDetail("errorCount", creationErrors.size());
-        addDetail("items", creationErrors);
+
+        List<ValidationError> errors = creationErrors.stream()
+                .map(ex -> ValidationError.builder()
+                        .errorType(ex.getClass().getSimpleName())
+                        .errorMessage(ex.getMessage())
+                        .additionalInfo(ex.getDetails().isEmpty() ? null : ex.getDetails())
+                        .build())
+                .toList();
+
+        addDetail("errorCount", errors.size());
+        addDetail("errors", errors);
     }
 
     @Data
     @Builder
-    public static class OrderItemError {
-        private String errorCode;
+    public static class ValidationError {
+        private String errorType;
         private String errorMessage;
         private Map<String, Object> additionalInfo;
     }
